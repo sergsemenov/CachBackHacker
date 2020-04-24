f2c4f3394755cfaf0e4f6e8f71a95bf2d29b88be
Sergey Semenov <gzuppy@gmail.com>
2020-04-24 10:46:09 +0300

    Branch JUnit4 added

--- src/main/java/ru/netology/service/CashbackService.java
+++ src/main/java/ru/netology/service/CashbackService.java
@@ -1,6 +1,6 @@
 package ru.netology.service;
 
-public class CashbackService {
+public class Cashbackgit commitService {
     private final int boundary = 1000;
 
     public int remain(int amount) {
--- src/test/java/ru/netology/service/CashbackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackServiceTest.java
@@ -1,41 +1,43 @@
 package ru.netology.service;
 
-import static org.testng.Assert.assertEquals;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class CashbackServiceTest {
 
-    @org.testng.annotations.Test
+    @Test
     public void shouldReturn1000for0() {
         CashbackService service = new CashbackService();
 
-        assertEquals(service.remain(0), 1000);
+        assertEquals( 1000, service.remain(0));
     }
 
-    @org.testng.annotations.Test
+    @Test
     public void shouldReturn100For900() {
         CashbackService service = new CashbackService();
 
-        assertEquals(service.remain(100), 900);
+        assertEquals(900, service.remain(100));
     }
 
-    @org.testng.annotations.Test
+    @Test
     public void shouldReturn999For1() {
         CashbackService service = new CashbackService();
 
-        assertEquals(service.remain(1), 999);
+        assertEquals(999, service.remain(1));
     }
 
-    @org.testng.annotations.Test
+    @Test
     public void shouldReturn999For1001() {
         CashbackService service = new CashbackService();
 
-        assertEquals(service.remain(1001), 999);
+        assertEquals(999, service.remain(1001));
     }
 
-    @org.testng.annotations.Test
+    @Test
     public void shouldReturn0For1000() {
         CashbackService service = new CashbackService();
 
-        assertEquals(service.remain(1000), 0);
+        assertEquals(0, service.remain(1000));
     }
 }
